40f0fb314b2371ee85a60a7f3ad4a1c931c3a880
tsutou <tsutoutakehara@gmail.com>
2022-01-29 14:40:31 +0900

    add result & error

---
+++ lib/data/model/result.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'app_error.dart';
+
+part 'result.freezed.dart';
+
+@freezed
+abstract class Result<T> with _$Result<T> {
+  const Result._();
+
+  const factory Result.success({required T data}) = Success<T>;
+
+  const factory Result.failure({required AppError error}) = Failure<T>;
+
+  bool get isSuccess => when(success: (data) => true, failure: (e) => false);
+
+  bool get isFailure => !isSuccess;
+
+  void ifSuccess(Function(T data) body) {
+    maybeWhen(
+      success: (data) => body(data),
+      orElse: () {
+        // no-op
+      },
+    );
+  }
+
+  void ifFailure(Function(AppError e) body) {
+    maybeWhen(
+      failure: (e) => body(e),
+      orElse: () {
+        // no-op
+      },
+    );
+  }
+
+  T get dataOrThrow {
+    return when(
+      success: (data) => data,
+      failure: (e) => throw e,
+    );
+  }
+}
